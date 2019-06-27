efedbd4324003a20cb55b5d92918300a1f4b5ccc
mhemans <m.hemans@technord.com>
2019-06-27 17:40:44 +0200

    Crée une annonce - Form - Working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnnonceComponent } from './annonce/annonce.component';
@@ -19,6 +19,7 @@ import { AjouterAnnonceComponent } from './ajouter-annonce/ajouter-annonce.compo
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule
   ],
